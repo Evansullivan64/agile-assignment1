9cb0f1528c8ca107fc96c79d7b8f0b1b7c3738ac
Evan Sullivan <20093137@mail.wit.ie>
2023-10-30 20:33:40 +0000

    added doc for support methods in support/e2e.js

--- movies/cypress/support/e2e.js
+++ movies/cypress/support/e2e.js
@@ -1,5 +1,27 @@
+
+
+// Function: filterByTitle
+//   Description:
+//    This function filters a movie list based on a provided string in the movie titles.
+
+//  Inputs:
+//    movieList (Array): A list of movies to be filtered.
+//    string (String): The string to be searched within the movie titles.
+//  Outputs:
+//    Returns a filtered array containing movies whose titles include the provided string.
 export const filterByTitle = (movieList, string) =>
   movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
 
+
+
+// Function: filterByGenre
+//  Description:
+//    This function filters a movie list based on a provided genre ID.
+
+//  Inputs:
+//    movieList (Array): A list of movies to be filtered.
+//    genreId (Number): The ID of the genre for filtering the movies.
+//  Outputs:
+//    Returns a filtered array containing movies belonging to the specified genre ID.
 export const filterByGenre = (movieList, genreId) =>
   movieList.filter((m) => m.genre_ids.includes(genreId));
